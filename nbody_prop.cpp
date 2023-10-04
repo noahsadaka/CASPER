b40f2effafd7fe9e5dc8034de374e93f28bd0461
Noah Sadaka <noahsadaka@gmail.com>
2023-10-03 22:52:22 -0400

    cr3bp system class

--- nbody_prop.cpp
+++ nbody_prop.cpp
@@ -20,6 +20,18 @@
 using namespace std; 
 namespace py = pybind11;
 
+class cr3bp_system{
+	public:
+		double m_star;
+		double l_star;
+		double t_star;
+	cr3bp_system(const double m, const double l, const double t){
+		m_star = m;
+		l_star = l;
+		t_star = t;
+	};
+};
+
 class nbody{
 
 };
@@ -33,12 +45,17 @@ PYBIND11_MODULE(casper, m) {
 	    .def_readonly("ID", &SpiceBody::ID)
 	    .def_readonly("mu", &SpiceBody::mu);
 
-    m.def("load_kernels", &load_kernels);
-
+    //m.def("load_kernels", &load_kernels);
     m.def("str2et", &str2et);
 
     py::class_<nbody>(m, "nbody")
 	    .def(py::init<>());
+
+    py::class_<cr3bp_system>(m, "cr3bp_system")
+	    .def(py::init<const double &, const double &, const double &>())
+	    .def_readonly("m_star", &cr3bp_system::m_star)
+	    .def_readonly("l_star", &cr3bp_system::l_star)
+	    .def_readonly("t_star", &cr3bp_system::t_star);
 }
 
 
